ad1e5dd55a4339660c9f3ca30647b224421d43b5
Manali B <manalibhattacharyya10@gmail.com>
2024-01-31 20:12:36 -0700

    Ex 2 - Commented out index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
