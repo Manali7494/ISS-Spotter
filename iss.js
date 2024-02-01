d4c0ae8379ad5802fabd733ea0dde3fd88bdbfbc
Manali B <manalibhattacharyya10@gmail.com>
2024-01-31 21:01:39 -0700

    Ex 4 - Missing Nested function

--- iss.js
+++ iss.js
@@ -96,23 +96,19 @@ const fetchISSFlyOverTimes = function(coords, callback) {
  *     [ { risetime: <number>, duration: <number> }, ... ]
  */
 const nextISSTimesForMyLocation = function(callback) {
-    fetchMyIP((error, ip) => {
-      if (error) {
-        return callback(error, null);
-      }
-  
-      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+      fetchCoordsByIP(ip, (error, loc) => {
         if (error) {
           return callback(error, null);
         }
-        fetchCoordsByIP(ip, (error, loc) => {
-            if (error) {
+  
+        fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+          if (error) {
             return callback(error, null);
-            }
+          }
+  
           callback(null, nextPasses);
         });
       });
-    });
   };
   
   // Only export nextISSTimesForMyLocation and not the other three (API request) functions.
