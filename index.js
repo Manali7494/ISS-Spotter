17b18e5d5275da1991745b4b65cce25dea11d31b
Manali B <manalibhattacharyya10@gmail.com>
2024-01-31 20:29:06 -0700

    Ex 3 - Correct and only contains necessary imports

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned coordinates:' , coordinates);
-// });
 
 // The code below is temporary and can be commented out.
 const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
--- iss.js
+++ iss.js
@@ -71,11 +71,19 @@ const fetchISSFlyOverTimes = function(coords, callback) {
     const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
   
     request(url, (error, response, body) => {
-
+      if (error) {
+        callback(error, null);
+        return;
+      }
+  
+      if (response.statusCode !== 200) {
+        callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+        return;
+      }
   
       const passes = JSON.parse(body).response;
       callback(null, passes);
     });
   };
   
-  module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+  module.exports = { fetchISSFlyOverTimes };
