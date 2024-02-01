34f0ba7f84e561c7818618ed28e672f98f2e84ef
Manali B <manalibhattacharyya10@gmail.com>
2024-01-31 20:28:22 -0700

    Ex 3 - Missing error status handling

--- iss.js
+++ iss.js
@@ -68,18 +68,10 @@ const fetchCoordsByIP = function(ip, callback) {
  *     [ { risetime: 134564234, duration: 600 }, ... ]
  */
 const fetchISSFlyOverTimes = function(coords, callback) {
-    const url = `https://iss-flyover.herokuapp.com/json/?lat=lon=`;
+    const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
   
     request(url, (error, response, body) => {
-      if (error) {
-        callback(error, null);
-        return;
-      }
-  
-      if (response.statusCode !== 200) {
-        callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
-        return;
-      }
+
   
       const passes = JSON.parse(body).response;
       callback(null, passes);
