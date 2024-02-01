16ceec67077b69cf6c38cb02d88fd1ad22991864
Manali B <manalibhattacharyya10@gmail.com>
2024-01-31 20:05:33 -0700

    Exercise 1 - Correct solution with commented index

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
