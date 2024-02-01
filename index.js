e9ec90f0690bdb50b4f7c72fda54faa297ad9bce
Manali B <manalibhattacharyya10@gmail.com>
2024-01-31 20:12:50 -0700

    Ex 2 - Both not commented index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned IP:' , ip);
-// });
+  console.log('It worked! Returned IP:' , ip);
+});
 
-// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned coordinates:' , coordinates);
-// });
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
